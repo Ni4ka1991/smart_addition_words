70a43834c4c798a6e9a2346d633bfbc24efcfcc2
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-24 13:13:27 +0200

    View text on page 1

--- app.py
+++ app.py
@@ -11,10 +11,24 @@ def textFromPDF( path, start = 0, end = 1000 ):
     text = ""                                    #initialisation var text string
     
     pdfFileObj = open( path, 'rb' )                    #open file pwd = path
+    my_pdf = PdfFileReader( pdfFileObj )
     
-    getPdfFile = PdfFileReader( pdfFileObj )
-    
+    info   = my_pdf.getDocumentInfo()
+    pages  = my_pdf.getNumPages()
+    page_1 = my_pdf.getPage( 0 )
+    text_1 = page_1.extractText()
+
     system( "clear" )
-    print( f"PDF: found {getPdfFile.numPages} pages" )
+    print( f"PDF: found {pages} pages" )
+    print( f"Meta-info: {info}" )
+    print()
+    print()
+    print( "*" * 20 + " >>> " +"TEXT page 1" + " <<< " + "*" * 20 )
+    print()
+    print( text_1 )
+    print()
+    print( "*" * 18 + " >>> " +"END TEXT" + " <<< " + "*" * 18 )
+    print()
+#    print( my_pdf.extractText() )
 
 textFromPDF( "./data/Pride-and-Prejudice.pdf", 0, 100 )

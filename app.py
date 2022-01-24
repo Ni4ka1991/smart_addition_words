e78704a65ae36e81ffd9f5bef1a169df88252037
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-24 15:09:30 +0200

    Extract text without unalphabet symbols

--- app.py
+++ app.py
@@ -13,22 +13,37 @@ def textFromPDF( path, start = 0, end = 1000 ):
     pdfFileObj = open( path, 'rb' )                    #open file pwd = path
     my_pdf = PdfFileReader( pdfFileObj )
     
-    info   = my_pdf.getDocumentInfo()
     pages  = my_pdf.getNumPages()
-    page_1 = my_pdf.getPage( 0 )
-    text_1 = page_1.extractText()
+
+    for i in range( start, min( pages, end ) ): # in range( 0 = start, min( num of pages and end == 1000 )
+        page = my_pdf.getPage( i )
+        text += page.extractText()
+        i += 1
+
+    textProcessed = ""
+
+    for i in text.lower():
+        c = alphabet.find( i )
+        if c == -1:
+            textProcessed += " "
+        else:
+            textProcessed += i
+
+
+
+
+
 
     system( "clear" )
     print( f"PDF: found {pages} pages" )
-    print( f"Meta-info: {info}" )
     print()
     print()
     print( "*" * 20 + " >>> " +"TEXT page 1" + " <<< " + "*" * 20 )
     print()
-    print( text_1 )
+    print( textProcessed )
     print()
     print( "*" * 18 + " >>> " +"END TEXT" + " <<< " + "*" * 18 )
     print()
 #    print( my_pdf.extractText() )
 
-textFromPDF( "./data/Pride-and-Prejudice.pdf", 0, 100 )
+textFromPDF( "./data/Pride-and-Prejudice.pdf", 0, 1 )

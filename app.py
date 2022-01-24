f9b07a97c54dbf0ae8968682b1e3e48c4fca8451
Ni4ka1991 <astahova_nica@mail.ru>
2022-01-24 12:24:19 +0200

    Read num of pdf-file pages

--- app.py
+++ app.py
@@ -3,12 +3,18 @@
 
 from PyPDF2 import PdfFileReader
 import re
+from os import system
 
 alphabet = ' abcdefghijklmnopqrstuvwxyz'
 
 def textFromPDF( path, start = 0, end = 1000 ):
     text = ""                                    #initialisation var text string
-    pdfFileObj = open( path )                    #open file pwd = path
-    pdfFileObj.read()
+    
+    pdfFileObj = open( path, 'rb' )                    #open file pwd = path
+    
+    getPdfFile = PdfFileReader( pdfFileObj )
+    
+    system( "clear" )
+    print( f"PDF: found {getPdfFile.numPages} pages" )
 
 textFromPDF( "./data/Pride-and-Prejudice.pdf", 0, 100 )

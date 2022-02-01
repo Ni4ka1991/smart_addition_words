f9ac2d525d9abe7c5bdbdb1f9d12b8261a62c965
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-02-01 15:36:22 +0200

    Some changes

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ import torch
 X = torch.tensor( X_, dtype = torch.float32 )
 Y = torch.tensor( Y_, dtype = torch.float32 )
 
-system( "clear" )
-print("TENSORS")
-print(X)
-print(X.shape)
-print(Y)
-print(Y.shape)
-
 #prepare the model
 N = 2 * len( vocabulary ) + len( alphabet )
 M = len( vocabulary )
@@ -22,6 +15,28 @@ D = 256
 
 rate = 0.01
 
+model = torch.nn.Sequential(
+        #input neurons
+        torch.nn.Linear( N, D ) # N = X[i]
+        torch.nn.ReLU( inplace = True )
+        torch.nn.Dropout( p = 0.2 )         #dropuot randomly zeroes some of the elements of the input tensor with probability p
+        
+        torch.nn.Linear( D, D )             #first from four deep layers
+        torch.nn.ReLU( inplace = True )
+        
+        torch.nn.Linear( D, D )             #2
+        torch.nn.ReLU( inplace = True )
+        torch.nn.Dropout( p = 0.5 )
+        
+        torch.nn.Linear( D, D )             #3
+        torch.nn.ReLU( inplace = True )
+        torch.nn.Dropout( p = 0.5 )
+        
+        torch.nn.Linear( D, D )             #4
+        torch.nn.ReLU( inplace = True )
+        
+        torch.nn.Linear( D, M )             #output neurons
+        torch.nn.LogSoftmax( dim = 1 )
 
 
 
